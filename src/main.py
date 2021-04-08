212257e427282e4820e795a7c77315f0c05b9df2
LucasDM1 <lucas19651@hotmail.com>
2021-04-08 22:33:21 +0000

    Relationship added

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Character
+from models import db, User, Character, Planet
+
 import json
 #from models import Person
 
@@ -31,6 +32,7 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def get_user():
     return jsonify(User.getAllusers()), 200
@@ -40,6 +42,10 @@ def new_user():
     request_body_user=request.data
     decoded_object = json.loads(request_body_user)
     return jsonify(User.create_user(decoded_object)), 200
+
+# @app.route('/user/<int:id>', methods=['DELETE'])
+# def delete_user():
+#     return jsonify(User.deleteUser(id)), 200
 
 @app.route('/character', methods=['GET'])
 def get_character():
@@ -51,6 +57,9 @@ def new_character():
     decoded_object = json.loads(request_body_char)
     return jsonify(Character.create_character(decoded_object)), 200
 
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    return jsonify(Planet.getAllplanets()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,8 +3,11 @@ from flask import Flask, request, jsonify, url_for
 
 db = SQLAlchemy()
 
+fav_char = db.Table('fav_chars', db.metadata, db.Column("user_id", db.Integer, db.ForeignKey("users.id")), db.Column("char_id", db.Integer, db.ForeignKey("characters.id")))
+fav_planet = db.Table('fav_planets', db.metadata, db.Column("user_id", db.Integer, db.ForeignKey("users.id")), db.Column("plan_id", db.Integer, db.ForeignKey("planets.id")))
+
 class User(db.Model):
-    
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     lastname = db.Column(db.String(250), nullable=False)
@@ -12,6 +15,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    characters = db.relationship("Character", secondary=fav_char) 
+    planets = db.relationship("Planet", secondary=fav_planet) 
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -21,6 +26,8 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "username":self.username,
+            "characters": list(map(lambda x: x.serialize(), self.characters)),
+            "planets": list(map(lambda x: x.serialize(), self.planets))
             # do not serialize the password, its a security breach
         }
     
@@ -30,20 +37,29 @@ class User(db.Model):
         return(all_users)
     
     def create_user(request_body_user):
-        user1 = User(name=request_body_user["name"], lastname=request_body_user["lastname"], username=request_body_user["username"], email=request_body_user["email"], password=request_body_user["password"])
-        db.session.add(user1)
+        user = User(name=request_body_user["name"], lastname=request_body_user["lastname"], username=request_body_user["username"], email=request_body_user["email"], password=request_body_user["password"])
+        db.session.add(user)
         db.session.commit()
         return("An user has been added")
 
+    def deleteUser(id):
+        user = User.query.get(id)
+        if user is None:
+            raise APIException('User not found', status_code=404)
+        db.session.delete(user)
+        db.session.commit()
+        return("The user has been deleted")
+
 class Character(db.Model):
-    
+    __tablename__='characters'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    birth_year = db.Column(db.Integer, nullable=False)
-    gender=db.Column(db.String(250), nullable=False)
-    height = db.Column(db.Integer, nullable=False)
-    eye_color=db.Column(db.String(250), nullable=False)
-    hair_color=db.Column(db.String(250), nullable=False)
+    # birth_year = db.Column(db.Integer, nullable=False)
+    # gender=db.Column(db.String(250), nullable=False)
+    # height = db.Column(db.Integer, nullable=False)
+    # eye_color=db.Column(db.String(250), nullable=False)
+    # hair_color=db.Column(db.String(250), nullable=False)
+    #users = relationship("User", secondary=fav_char back_populates="favoriteChars") 
 
     
 
@@ -60,9 +76,37 @@ class Character(db.Model):
         return(all_characters)
 
     def create_character(request_body_char):
-        char1 = Character(name=request_body_char["name"], birth_year=request_body_char["birth_year"], gender=request_body_char["gender"], height=request_body_char["height"], eye_color=request_body_char["eye_color"], hair_color=request_body_char["hair_color"])
+        char1 = Character(name=request_body_char["name"])
+        #, birth_year=request_body_char["birth_year"], gender=request_body_char["gender"], height=request_body_char["height"], eye_color=request_body_char["eye_color"], hair_color=request_body_char["hair_color"])
         db.session.add(char1)
         db.session.commit()
         return("A character has been added")
+
+class Planet(db.Model):
+    __tablename__='planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    #users = relationship("User", secondary=fav_planet back_populates="favoritePlanets") 
+
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+    
+    def getAllplanets():
+        planets_query = Planet.query.all()
+        all_planets = list(map(lambda x: x.serialize(), planets_query))
+        return(all_planets)
+
+    def create_planet(request_body_planet):
+        planet = Planet(name=request_body_planet["name"])
+        #, birth_year=request_body_char["birth_year"], gender=request_body_char["gender"], height=request_body_char["height"], eye_color=request_body_char["eye_color"], hair_color=request_body_char["hair_color"])
+        db.session.add(planet)
+        db.session.commit()
+        return("A planet has been added")
     
     
